ee8eb5cdc84fe8df789ce125e08f9f9b603c1cb8
Martinsse Alexandre <martinsse.alexandre@gmail.com>
2026-01-31 16:12:13 +0100

    Update mask.gd

--- scenes/game/mask.gd
+++ scenes/game/mask.gd
@@ -7,6 +7,7 @@ extends Node2D
 
 var _elements: Array[MaskElement] = []
 
+
 func _ready() -> void:
 	_sprite.texture = base_texture
 
@@ -15,37 +16,57 @@ func add_element(_element):
 	_elements.append(_element)
 	add_child(_element)
 
-
 func remove_last_element():
 	if _elements.is_empty():
 		return
 	_elements.pop_back()
-	
+
 func hide_mask():
 	var tween = get_tree().create_tween()
 	tween.tween_property($Sprite2D, "position", Vector2(0, 1000), 1.0)
-	
+
 func show_mask():
 	var tween = get_tree().create_tween()
 	tween.tween_property($Sprite2D, "position", Vector2(0, 0), 1.0)
-	
+
 func reset_mask():
 	_elements = [] # Marche peut-être
 	print("Waow, tu as reset le masque !")
 	
 func undo():
 	print("NAN !")
+
+func alea_demande():
+	pass
+
+func nouvelles_demandes():
+	pass
+
+func evaluer_masque():
+	pass
+	# Couleur
+	# Yeux
+	# Bouche
+	# Oreilles
+	# Cornes/Décorations
+	# Reste
 	
+
+
+# Réactions aux signaux
+
 func _on_main_cacher_masque() -> void:
 	hide_mask()
 	await get_tree().create_timer(1).timeout
+	evaluer_masque()
+	await get_tree().create_timer(1).timeout
 	reset_mask()
+	nouvelles_demandes()
 	await get_tree().create_timer(1).timeout
 	show_mask()
-	
+
 func _on_main_reset_masque() -> void:
 	reset_mask()
-
 
 func _on_main_undo_masque() -> void:
 	undo()
