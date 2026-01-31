6f6b5a31d9cde21ad6457630756a0a1afc06af4d
Yithash2 <147191554+Yithash2@users.noreply.github.com>
2026-01-31 14:46:26 +0100

    kfykhsvvk skhv souv

---
+++ globals/mask_element_manager.gd
@@ -0,0 +1,17 @@
+class_name MaskElementManager
+extends Node2D
+
+var left_click_just_pressed : bool
+var current_mask_element : MaskElement 
+
+func set_mask_element(elem : MaskElement):
+	if current_mask_element :
+		current_mask_element = elem
+	else :
+		current_mask_element.destroy_self()
+		current_mask_element = elem
+		
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouseButton:
+		left_click_just_pressed = event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT
--- scenes/ui/mask_element_ui.gd
+++ scenes/ui/mask_element_ui.gd
@@ -1,11 +1,12 @@
-extends Node2D
+extends Button
 
 @export var base_texture : Texture
 
-@onready var _button : Button = $Button
-@onready var _mask_element : MaskElement = $Button/MaskElement
+@onready var _mask_element : MaskElement = $MaskElement
 
 @export var _element_param : MaskElementParam
+
+var _mask_element_scene = load("res://scenes/game/MaskElement.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+func _on_pressed() -> void:
+	var s = _mask_element_scene.instantiate()
+	s.base_texture = _element_param.base_texture
+	GlobalMaskElement.set_mask_element(s)
