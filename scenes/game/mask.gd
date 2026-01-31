13868a24386139adceb7705118e81137747b15a9
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2026-01-31 12:08:45 +0100

    Add and remove elmebts caca pipi

---
+++ scenes/game/mask.gd
@@ -0,0 +1,23 @@
+class_name Mask
+extends Node2D
+
+@export var base_texture: Texture2D
+
+@onready var _sprite: Sprite2D = $Sprite2D
+
+var _elements: Array[MaskElement] = []
+
+func _ready() -> void:
+	_sprite.texture = base_texture
+
+
+func add_element(_element):
+	_elements.append(_element)
+	add_child(_element)
+
+
+func remove_last_element():
+	if _elements.is_empty():
+		return
+	_elements.pop_back()
+	
