3ae6297ba0ec160950c60ea8fe4db590a99c15b0
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2026-02-01 16:14:04 +0100

    title

--- scenes/tutorial.gd
+++ scenes/tutorial.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var lore_scene = load("res://scenes/tutorial.tscn")
+var lore_scene = load("res://scenes/main.tscn")
 
 func _on_button_pressed() -> void:
 	TransitionManager.change_scene(lore_scene)
